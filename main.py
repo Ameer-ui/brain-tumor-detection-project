21005ed2d76289b16f952412429dab41dd772c38
Ameer-ui <ameersmaniyar@gmail.com>
2025-11-16 11:30:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from test_model_training import train_model
 from test_prediction import predict_and_visualize
 
 if __name__ == "__main__":
-    train_loader, test_loader, class_names = load_data()
-    model = train_model(train_loader, test_loader)
-    predict_and_visualize(model, test_loader)
+    train_generator, test_generator, class_names = load_data()
+    model = train_model(train_generator, test_generator)
+    predict_and_visualize(model, test_generator)
